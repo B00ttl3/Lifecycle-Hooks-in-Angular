35e051b4c5e86df7626c3b1e802398ee94843e8e
B00ttl3 <mumair3825@gmail.com>
2024-10-04 01:07:54 +0500

    Just rendered the data from posts-list html to view programatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,32 @@ import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+import { NgComponentOutlet, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+import { PostsListComponent } from './posts-list/posts-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule, NgIf, NgTemplateOutlet, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
+  imports: [
+    RouterOutlet, 
+    AppNavbar, 
+    HeaderComponent, 
+    FormsModule, 
+    NgIf, 
+    NgTemplateOutlet, 
+    NgFor, 
+    NgSwitch, 
+    NgSwitchCase, 
+    NgSwitchDefault, 
+    PostsListComponent,
+    NgComponentOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title: string = 'This loaded dynamically'
 
+  loadComponent(){
+    return PostsListComponent;
+  }
 
 }
