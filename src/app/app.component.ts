59901938642f34fe380e6fbaf19a06d2a1a705ed
B00ttl3 <mumair3825@gmail.com>
2024-10-03 23:24:25 +0500

    Started learning about Lifecycle hooks in Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule, NgIf, NgTemplateOutlet, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title: string = 'This loaded dynamically'
+
+
+}
